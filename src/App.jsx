0e8bf87cb6e644db752b2f396730273412abe788
sheetal subbannavar <sheetalssubbbu@gmail.com>
2025-09-25 23:22:20 +0530

    First version of Bharat Clock

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ClockHeading from '../components/ClockHeading.jsx';
+import ClockSlogan from '../components/ClockSlogan.jsx';
+import CurrentTime from '../components/CurrentTime.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+function App() {
+  
+
+  return (
+    <center>
+      <ClockHeading></ClockHeading>
+      <ClockSlogan></ClockSlogan>
+      <CurrentTime></CurrentTime>
+    </center>
+  );
+}
+
+export default App
